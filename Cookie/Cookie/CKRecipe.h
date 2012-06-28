89b040d44432519ebb827cd3a8e6fcfe70566a59
Benjamin Lefebvre <benjamin@lefebvre.com>
2012-06-28 23:17:02 +0200

    * project target updated 10.7 -> 10.6 + recipe model

---
+++ Cookie/Cookie/CKRecipe.h
@@ -0,0 +1,29 @@
+//
+//  CKRecipe.h
+//  Cookie
+//
+//  Created by Benjamin Lefebvre on 6/28/12.
+//  Copyright (c) 2012 3IE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKRecipe : NSObject {
+    NSString* uniqueID;
+    NSString* name;
+    NSNumber* category;
+    NSString* pictureID;
+    NSNumber* rating;
+    NSData* summary;
+    NSArray* ingredients;
+}
+
+@property (nonatomic, retain) NSString* uniqueID;
+@property (nonatomic, retain) NSString* name;
+@property (nonatomic, retain) NSNumber* category;
+@property (nonatomic, retain) NSString* pictureID;
+@property (nonatomic, retain) NSNumber* rating;
+@property (nonatomic, retain) NSData* summary;
+@property (nonatomic, retain) NSArray* ingredients;
+
+@end
---
+++ Cookie/Cookie/CKRecipe.m
@@ -0,0 +1,15 @@
+//
+//  CKRecipe.m
+//  Cookie
+//
+//  Created by Benjamin Lefebvre on 6/28/12.
+//  Copyright (c) 2012 3IE. All rights reserved.
+//
+
+#import "CKRecipe.h"
+
+@implementation CKRecipe
+
+@synthesize uniqueID, name, category, pictureID, rating, summary, ingredients;
+
+@end
