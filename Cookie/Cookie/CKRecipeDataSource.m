3997b486adbcfe38fa96377ba70b317d2752b8f5
Eugeni Cedric <cedric.eugeni@epita.fr>
2012-07-03 11:47:09 +0200

    changed behavior of searching. It now works with ingredients instead of recipe name. And logic should be working.
    	modified:   Cookie/Cookie/CKRecipeDataSource.m
    	modified:   Cookie/Cookie/CKRecipesViewController.m

--- Cookie/Cookie/CKRecipeDataSource.m
+++ Cookie/Cookie/CKRecipeDataSource.m
@@ -21,13 +21,13 @@
         NSData *data = [[NSData alloc] init];
         NSNumber *rating = [NSNumber numberWithInt:4];
         
-        CKRecipe *recipe = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"Yop" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[NSArray arrayWithObject:@"Pomme"]];
+        CKRecipe *recipe = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"Yop" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[[NSArray arrayWithObject:@"Pomme"] retain]];
         
-        CKRecipe *recipe2 = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"Bonjour" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[NSArray arrayWithObject:@"Pomme"]];
+        CKRecipe *recipe2 = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"Bonjour" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[[NSArray arrayWithObject:@"Pomme"] retain]];
         
-        CKRecipe *recipe3 = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"CreveBitch" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[NSArray arrayWithObject:@"Pomme"]];
+        CKRecipe *recipe3 = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"CreveBitch" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[[NSArray arrayWithObject:@"Pomme"] retain]];
         
-        CKRecipe *recipe4 = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"CrevePute" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[NSArray arrayWithObject:@"Pomme"]];
+        CKRecipe *recipe4 = [[CKRecipe alloc] initWithUniqueID:@"124332" andName:@"CrevePute" andCategory:rating andPictureID:@"1243" andRating:rating andSummary:data andIngredients:[[NSArray arrayWithObject:@"Pomme"] retain]];
         [items addObject:recipe];
         [items addObject:recipe2];
         [items addObject:recipe3];
